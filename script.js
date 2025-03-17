ab4350f52fef82905d33e25c04b51aa87d416bea
mauroanastasi <mauroanastasi1@gmail.com>
2025-03-17 16:05:29 +0100

    [UPD] creo i file necessari, inizializzo le prime variabili con i prompt

---
+++ script.js
@@ -0,0 +1,5 @@
+// permetto di inserire al'utente l'età e il numero dei km che vuole percorrere
+let age = prompt("Inserisci la tua età:");
+let kmCount = prompt("Inserisci i chilometri che vuoi percorrere:")
+
+
